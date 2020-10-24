275ddc8d1546ca7c7a5ab25194ced508f979f97a
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-24 10:57:52 +0900

    Revert "revert後再度コミット"
    This reverts commit e68eac96f16fad3a588cf812bd63551127a8b956.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_week
     @plan = Plan.new
   end
 
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def getWeek
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -34,15 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< HEAD
       days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
-=======
-      wday_num = Date.today.wday + x
-      if wday_num >= 7
-        wday_num = wday_num - 7
-      end 
-      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
->>>>>>> parent of 2cc4809... Merge branch 'master' into second_training
       @week_days.push(days)
     end
 
